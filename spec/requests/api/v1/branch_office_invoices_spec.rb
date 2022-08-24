f9336eda2159de7f0977c78965ce0d0ba2512896
carloscodify <carlos.gutierrez@codify.com.bo>
2022-08-24 15:50:19 -0400

    refactor invoice, invoice_detail model and rspec model

--- spec/requests/api/v1/branch_office_invoices_spec.rb
+++ spec/requests/api/v1/branch_office_invoices_spec.rb
@@ -5,6 +5,14 @@ require 'rails_helper'
 RSpec.describe '/api/v1/branch_offices/:branch_office_id/invoices', type: :request do
   let(:valid_attributes) do
     {
+      # municipality: 'Santa Cruz',
+      # phone: '12345',
+      # number: '1',
+      # address: 'por ahi',
+      # point_of_sale: 1,
+      # date: '2022-01-01',
+      # total: 100,
+
       business_name: 'Juan Perez',
       document_type: 1,
       business_nit: '1234567',
