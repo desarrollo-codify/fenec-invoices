6dc264e6dde18ac16b66c4db4f44f51e328b5ebe
carloscodify <carlos.gutierrez@codify.com.bo>
2022-09-03 14:06:55 -0400

    Changes

--- app/controllers/api/v1/contingencies_controller.rb
+++ app/controllers/api/v1/contingencies_controller.rb
@@ -34,7 +34,7 @@ module Api
         
         if @contingency.save
           Contingency.perform_now(@contingency)
-          ReceptionValidation(@contingency)
+          reception_validation(@contingency)
           render json: @contingency, status: :created
         else
           render json: @contingency.errors, status: :unprocessable_entity
@@ -69,6 +69,51 @@ module Api
       # Only allow a list of trusted parameters through.
       def contingency_params
         params.require(:contingency).permit(:start_date, :end_date, :significative_event_id)
+      end
+
+      def reception_validation(contingency)
+        cuis_code = contingency.branch_offices.cuis_codes.last
+        cufd_code = contingency.branch_offices.daily_codes.last
+        client =Savon.client(
+          wsdl: ENV.fetch('siat_invoices'.to_s, nil),
+          headers: {
+            'apikey' => ENV.fetch('api_key', nil),
+            'SOAPAction' => ''
+          },
+          namespace: ENV.fetch('siat_namespace', nil),
+          convert_request_keys_to: :none
+        )
+    
+        body = {
+          SolicitudServicioValidacionRecepcionPaquete: {
+            codigoAmbiente: 2,
+            codigoSistema: ENV.fetch('system_code', nil),
+            codigoSucursal: branch_office.number,
+            nit: branch_office.company.nit.to_i,
+            codigoDocumentoSector: 1
+            codigoEmision: 2
+            codigoModalidad: 2
+            cufd: cufd_code.code,
+            cuis: cuis_code.code,
+            tipoFacturaDocumento: 1
+            codigoRecepcion: contingency.reception_code
+          }
+        }
+        response = client.call(:validacion_recepcion_paquete_factura, message: body)
+        if response.success?
+          data = response.to_array(:validacion_recepcion_paquete_factura_response, :respuesta_servicio_facturacion , :mensajes_list)
+          data = data[:codigoEstado]
+        else
+          data = {return: 'communication error'}
+        end
+        if data == '908'
+          data = 'valid'
+        else 
+          if data == '904'
+            data = 'observed'
+          else
+            data = 'pending' if data == '901'
+        end
       end
     end
   end
