cf9fd7f110f466d53a1093adc430442e7154469d
codify-dev <dev@codify.com.bo>
2022-09-12 15:59:18 -0400

    Update invoice status when cancelling

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -98,7 +98,7 @@ module Api
         CancelInvoiceJob.perform_later(@invoice)
 
         # TODO: send cancellation_reason_id in body
-        @invoice.update(cancellation_date: DateTime.now, cancellation_reason_id: 1)
+        @invoice.update(cancellation_date: DateTime.now, cancellation_reason_id: 1, invoice_status_id: 2)
         render json: @invoice
       end
 
