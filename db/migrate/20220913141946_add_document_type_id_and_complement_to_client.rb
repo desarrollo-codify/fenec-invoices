dc76f2aff5904d0d20ee439bc6d634be41d8df1d
carloscodify <carlos.gutierrez@codify.com.bo>
2022-09-13 10:25:03 -0400

    Add complement and document_type to Client

---
+++ db/migrate/20220913141946_add_document_type_id_and_complement_to_client.rb
@@ -0,0 +1,6 @@
+class AddDocumentTypeIdAndComplementToClient < ActiveRecord::Migration[7.0]
+  def change
+    add_column :clients, :complement, :string
+    add_reference :clients, :document_type, foreign_key: true
+  end
+end
