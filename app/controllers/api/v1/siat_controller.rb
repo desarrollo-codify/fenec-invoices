d60ea899a46b5cbfce7a6ec595cd88e594c4c861
codify-dev <dev@codify.com.bo>
2022-09-15 14:57:54 -0400

    Fix cancel invoice

--- app/controllers/api/v1/siat_controller.rb
+++ app/controllers/api/v1/siat_controller.rb
@@ -14,7 +14,7 @@ module Api
         body = {
           SolicitudCuis: {
             codigoAmbiente: 2,
-            codigoPuntoVenta: 1,
+            codigoPuntoVenta: 0,
             codigoSistema: @company.company_setting.system_code,
             nit: @company.nit.to_i,
             codigoModalidad: 2,
@@ -57,7 +57,7 @@ module Api
         body = {
           SolicitudCufd: {
             codigoAmbiente: 2,
-            codigoPuntoVenta: 1,
+            codigoPuntoVenta: 0,
             codigoSistema: @company.company_setting.system_code,
             nit: @company.nit.to_i,
             codigoModalidad: 2,
@@ -83,7 +83,7 @@ module Api
 
       def show_cufd
         @daily_code = @branch_office.daily_codes.current
-        if @daily_code
+        if @daily_code.present?
           render json: @daily_code
         else
           error_message = 'La sucursal no cuenta con un codigo diario CUFD.'
