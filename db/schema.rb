51fc9de5f352a0fb567cd8ef700473b6940fe4bb
carloscodify <carlos.gutierrez@codify.com.bo>
2022-09-19 17:16:21 -0400

    Refactor payment method "Gift-Card"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_130211) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_203003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -298,10 +298,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_130211) do
     t.bigint "cancellation_reason_id"
     t.datetime "emailed_at"
     t.string "graphic_representation_text"
+    t.boolean "cancel_sent_at"
     t.index ["branch_office_id"], name: "index_invoices_on_branch_office_id"
     t.index ["cancellation_reason_id"], name: "index_invoices_on_cancellation_reason_id"
     t.index ["invoice_status_id"], name: "index_invoices_on_invoice_status_id"
     t.index ["number", "cufd_code"], name: "index_invoices_on_number_and_cufd_code", unique: true
+  end
+
+  create_table "invoices_payment_methods", id: false, force: :cascade do |t|
+    t.bigint "invoice_id", null: false
+    t.bigint "payment_method_id", null: false
   end
 
   create_table "issuance_types", force: :cascade do |t|
@@ -310,12 +316,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_130211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_issuance_types_on_code", unique: true
-  end
-
-  create_table "jwt_denylist", force: :cascade do |t|
-    t.string "jti", null: false
-    t.datetime "exp", null: false
-    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "legends", force: :cascade do |t|
@@ -408,21 +408,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_130211) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
