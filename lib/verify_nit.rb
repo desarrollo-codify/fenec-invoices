ae91f7d5886c55a2e4230ee3e37e6b2c68d97f5e
codify-dev <dev@codify.com.bo>
2022-09-20 22:56:31 -0400

    Refactor verify nit and handle response errors

--- lib/verify_nit.rb
+++ lib/verify_nit.rb
@@ -1,38 +1,43 @@
 # frozen_string_literal: true
 
 class VerifyNit
+  require 'siat_available'
+  
   def self.verify(nit, branch_office)
-    cuis_code = branch_office.cuis_codes.where(point_of_sale: 0).current.code
+    result = true
+    if SiatAvailable.available(branch_office.invoices.last, false)
+      cuis_code = branch_office.cuis_codes.where(point_of_sale: 0).current.code
 
-    client = Savon.client(
-      wsdl: ENV.fetch('cuis_wsdl'.to_s, nil),
-      headers: {
-        'apikey' => branch_office.company.company_setting.api_key,
-        'SOAPAction' => ''
-      },
-      namespace: ENV.fetch('siat_namespace', nil),
-      convert_request_keys_to: :none
-    )
-    body = {
-      SolicitudVerificarNit: {
-        codigoAmbiente: 2,
-        codigoSistema: branch_office.company.company_setting.system_code,
-        codigoModalidad: 2,
-        nit: branch_office.company.nit.to_i,
-        cuis: cuis_code,
-        codigoSucursal: branch_office.number,
-        nitParaVerificacion: nit
+      client = Savon.client(
+        wsdl: ENV.fetch('cuis_wsdl'.to_s, nil),
+        headers: {
+          'apikey' => branch_office.company.company_setting.api_key,
+          'SOAPAction' => ''
+        },
+        namespace: ENV.fetch('siat_namespace', nil),
+        convert_request_keys_to: :none
+      )
+      body = {
+        SolicitudVerificarNit: {
+          codigoAmbiente: 2,
+          codigoSistema: branch_office.company.company_setting.system_code,
+          codigoModalidad: 2,
+          nit: branch_office.company.nit.to_i,
+          cuis: cuis_code,
+          codigoSucursal: branch_office.number,
+          nitParaVerificacion: nit
+        }
       }
-    }
-
-    response = client.call(:verificar_nit, message: body)
-
-    return unless response.success?
-
-    data = response.to_array(:verificar_nit_response, :respuesta_verificar_nit, :mensajes_list).first
-
-    description = data[:descripcion]
-
-    description == 'NIT ACTIVO'
+  
+      response = client.call(:verificar_nit, message: body)
+      return unless response.success?
+  
+      data = response.to_array(:verificar_nit_response, :respuesta_verificar_nit, :mensajes_list).first
+      description = data[:descripcion]
+      result = description == 'NIT ACTIVO'
+    else
+      result = true
+    end
+    result
   end
 end
