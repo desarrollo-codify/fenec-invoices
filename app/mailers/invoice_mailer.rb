1b3fbc803de82994eda9e68aa51f91af93e91862
carloscodify <carlos.gutierrez@codify.com.bo>
2022-10-05 17:54:36 -0400

    Add rspec of send invoice mailer

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -7,20 +7,18 @@ class InvoiceMailer < ApplicationMailer
   def send_invoice
     @client = params[:client]
     @invoice = params[:invoice]
-
     delivery_options = { user_name: params[:sender].user_name,
                          password: params[:sender].password,
                          domain: params[:sender].domain,
                          port: params[:sender].port,
                          address: params[:sender].address }
-
     xml_path = "#{Rails.root}/public/tmp/mails/#{@invoice.cuf}.xml"
     GenerateXmlJob.perform_now(@invoice) unless File.exist?(xml_path)
     attachments['factura.xml'] = File.read(xml_path)
 
     pdf_path = "#{Rails.root}/public/tmp/mails/#{@invoice.cuf}.pdf"
-    FileUtils.rm_f(pdf_path)
-    GeneratePdfJob.perform_now(@invoice)
+
+    GeneratePdfJob.perform_now(@invoice) unless File.exist?(pdf_path)
     attachments['factura.pdf'] = File.read(pdf_path)
 
     mail to: @client.email, subject: 'Factura', delivery_method_options: delivery_options
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_203327) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_165314) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_203327) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -121,7 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_203327) do
     t.integer "document_sector_code", null: false
     t.integer "limit", null: false
     t.integer "current_use", null: false
-    t.boolean "available", null: false
+    t.boolean "available"
     t.integer "economic_activity_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
