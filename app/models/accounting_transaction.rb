b838903228f0ec70670db940aee6c4f7a2bd49a4
carloscodify <carlos.gutierrez@codify.com.bo>
2022-11-02 16:40:58 -0400

    Add validates in accounting transaction

--- app/models/accounting_transaction.rb
+++ app/models/accounting_transaction.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class AccountingTransaction < ApplicationRecord
+  validates :date, presence: true
+  validates :gloss, presence: true
+
+  validate :has_at_least_two_entries
+  validate :debit_and_credit_equal_sum
+
   belongs_to :currency
   belongs_to :cycle
   belongs_to :company
@@ -8,4 +14,15 @@ class AccountingTransaction < ApplicationRecord
 
   has_many :entries, dependent: :destroy
   accepts_nested_attributes_for :entries, reject_if: :all_blank
+
+  private
+
+  def has_at_least_two_entries
+    errors.add(:entries, 'Se deben agregar un mínimo de dos asientos.') unless entries.count >= 2
+  end
+
+  def debit_and_credit_equal_sum
+    errors.add(:entries, 'La suma de debito y credito debe ser igual.') unless entries.sum(:debit_bs) == entries.sum(:credit_bs)
+    errors.add(:entries, 'La suma de debito y credito debe ser igual.') unless entries.sum(:debit_sus) == entries.sum(:credit_sus)
+  end
 end
--- spec/models/accounting_transaction_spec.rb
+++ spec/models/accounting_transaction_spec.rb
@@ -3,5 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe AccountingTransaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:currency) }
+  it { is_expected.to belong_to(:cycle) }
+  it { is_expected.to belong_to(:company) }
+  it { is_expected.to belong_to(:transaction_type) }
+  it { is_expected.to have_many(:entries) }
+  
+  let(:company) { create(:company) }
+  let(:currency) { create(:currency) }
+  let(:cycle) { create(:cycle, company: company) }
+  let(:transaction_type) { create(:transaction_type) }
+  subject { build(:accounting_transaction, company: company, currency: currency, cycle: cycle, transaction_type: transaction_type) }
+  let(:account_type) { create(:account_type) }
+  let(:account_level) { create(:account_level) }
+  let(:account) { create(:account, account_type: account_type, account_level: account_level, company: company, cycle: cycle) }
+  let(:accounting_transaction) { build(:accounting_transaction, company: company, currency: currency, cycle: cycle, transaction_type: transaction_type) }
+
+  describe 'with valid and invalid values' do
+    it 'is not valid' do
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid' do
+      accounting_transaction.entries.build(debit_bs: 10, account: account)
+      accounting_transaction.entries.build(credit_bs: 10, account: account)
+      expect(accounting_transaction).to be_valid
+    end
+  end
+
+  describe 'date attribute' do
+    it { validate_presence_of(:date) }
+
+    context 'with nil or empty value' do
+      let(:accounting_transaction) { build(:accounting_transaction, date: nil, company: company, currency: currency, cycle: cycle, transaction_type: transaction_type) }
+
+      it 'is invalid' do
+        accounting_transaction.entries.after_create(debit_bs: 10, account: account)
+        accounting_transaction.entries.after_create(credit_bs: 10, account: account)
+        expect(accounting_transaction).to_not be_valid
+        accounting_transaction.date = ''
+        expect(accounting_transaction).to_not be_valid
+      end
+    end
+  end
 end
