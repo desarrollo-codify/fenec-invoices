f4a33728df7d519d25e7ccd64ffd600774b94a92
carloscodify <carlos.gutierrez@codify.com.bo>
2022-12-13 16:41:44 -0400

    Add rspec payment model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -3,5 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:payment) }
+
+  describe 'with valid values' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'mount attribute' do
+    it { validate_presence_of(:mount) }
+
+    context 'with nil or empty value' do
+      let(:payment) { build(:payment, mount: nil) }
+
+      it 'is invalid' do
+        expect(payment).to_not be_valid
+        payment.mount = ''
+        expect(payment).to_not be_valid
+      end
+    end
+  end
+
+  describe 'invoice_id attribute' do
+    context 'not associated to a invoice' do
+      let(:payment) { build(:payment, invoice: nil) }
+
+      it 'is invalid' do
+        expect(payment).to_not be_valid
+      end
+    end
+  end
+
+  describe 'payment_method_id attribute' do
+    context 'not associated to a invoice' do
+      let(:payment) { build(:payment, payment_method: nil) }
+
+      it 'is invalid' do
+        expect(payment).to_not be_valid
+      end
+    end
+  end
 end
