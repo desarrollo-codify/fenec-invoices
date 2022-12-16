490a4f992fabd679c17ab3fba3324341fbf6a6d5
carloscodify <carlos.gutierrez@codify.com.bo>
2022-12-16 16:49:06 -0400

    Rubocoping

--- db/migrate/20221216190357_add_account_levels_to_company_setting.rb
+++ db/migrate/20221216190357_add_account_levels_to_company_setting.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAccountLevelsToCompanySetting < ActiveRecord::Migration[7.0]
   def change
     add_column :company_settings, :account_levels, :integer, default: 4
