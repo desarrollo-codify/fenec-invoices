ba8d8aadea1f3c5128434e221defc3b42e4c85c1
carloscodify <carlos.gutierrez@codify.com.bo>
2022-12-23 12:46:47 -0400

    Add a correlative number to account transactions by company, cycle and transaction type

--- app/models/transaction_type.rb
+++ app/models/transaction_type.rb
@@ -2,4 +2,6 @@
 
 class TransactionType < ApplicationRecord
   validates :description, presence: { message: 'La descripción no puede estar en blanco.' }
+
+  has_many :transaction_numbers, dependent: :destroy
 end
