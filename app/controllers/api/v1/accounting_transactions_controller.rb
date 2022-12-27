d9629dbb7f327756c4dd11213774712411cf63cb
carloscodify <carlos.gutierrez@codify.com.bo>
2022-12-26 23:37:40 -0400

    Refactor validations and update accounting transactions

--- app/controllers/api/v1/accounting_transactions_controller.rb
+++ app/controllers/api/v1/accounting_transactions_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class AccountingTransactionsController < ApplicationController
       before_action :set_company, only: %i[index create]
-      before_action :set_accounting_transaction, only: %i[show update destroy update_gloss]
+      before_action :set_accounting_transaction, only: %i[show update destroy]
 
       # GET /api/v1/companies/1/accounting_transactions
       def index
@@ -38,7 +38,7 @@ module Api
 
         if @accounting_transaction.save
           @accounting_transaction.accounting_transaction_logs.create(full_name: 'current_user.full_name', action: 'CREATE',
-                                                                     log_action: @accounting_transaction.as_json(include: :entires))
+                                                                     log_action: @accounting_transaction.as_json(include: :entries))
           render json: @accounting_transaction, status: :created
         else
           render json: @accounting_transaction.errors.full_messages, status: :unprocessable_entity
@@ -53,24 +53,8 @@ module Api
 
         if @accounting_transaction.update(accounting_transaction_params)
           @accounting_transaction.accounting_transaction_logs.create(full_name: 'current_user.full_name', action: 'UPDATE',
-                                                                     log_action: @accounting_transaction.as_json(include: :entires))
+                                                                     log_action: @accounting_transaction.as_json(include: :entries))
           render json: @accounting_transaction
-        else
-          render json: @accounting_transaction.errors.full_messages, status: :unprocessable_entity
-        end
-      end
-
-      # PATCH/PUT /api/v1/accounting_transactions/1/update_gloss
-      def update_gloss
-        @errors = []
-        validate_update!(@accounting_transaction, accounting_transaction_gloss_params, true)
-        return render json: @errors, status: :unprocessable_entity if @errors.any?
-
-        if @accounting_transaction.update(accounting_transaction_gloss_params)
-          render json: @accounting_transaction.as_json(include: [{ currency: { only: %i[id description abbreviation] } },
-                                                                 { cycle: { only: %i[id year] } },
-                                                                 { transaction_type: { only: %i[id description] } },
-                                                                 { entries: { except: %i[created_at updated_at] } }])
         else
           render json: @accounting_transaction.errors.full_messages, status: :unprocessable_entity
         end
@@ -82,10 +66,8 @@ module Api
         @errors << 'No se puede crear un comprobante si no existe una gestión abierta.' unless @company.cycles.current.present?
       end
 
-      def validate_update!(accounting_transaction, params, is_gloss)
+      def validate_update!(accounting_transaction, params, _is_gloss)
         @errors << 'No se puede editar un comprobante de gestiones cerradas.' unless accounting_transaction.cycle.status == 'ABIERTA'
-
-        return if is_gloss
 
         @errors << 'No se puede editar el número de comprobantes' if params[:number].present? && accounting_transaction.number != params[:number]
 
@@ -93,9 +75,42 @@ module Api
           @errors << 'No se puede editar el tipo de transacción una vez creado el comprobante.'
         end
 
-        return if accounting_transaction.date.to_date == Date.today
+        validate_date(accounting_transaction, params) unless accounting_transaction.date.to_date == Date.today
+      end
 
-        @errors << 'Para realizar cambios en un día distinto al de creación, se requiere un nuevo comprobante.'
+      def validate_date(accounting_transaction, params)
+        if accounting_transaction.date != params[:date].to_date
+          return @errors << 'Para realizar cambios en un día distinto al de creación, se requiere un nuevo comprobante.'
+        end
+        if accounting_transaction.receipt != params[:receipt]
+          return @errors << 'Para realizar cambios en un día distinto al de creación, se requiere un nuevo comprobante.'
+        end
+        if accounting_transaction.currency_id != params[:currency_id]
+          return @errors << 'Para realizar cambios en un día distinto al de creación, se requiere un nuevo comprobante.'
+        end
+
+        params[:entries_attributes].each do |entry|
+          id = entry[:id] if entry[:id].present?
+
+          return @errors << 'No se pueden añadir asientos nuevos un día distinto al de creación del comprobante.' unless id.present?
+
+          entry_current = Entry.find(id)
+          if entry[:debit_bs] != entry_current.debit_bs
+            return @errors << 'No se pueden realizar cambios en algun asiento un día distinto al de creación del comprobante.'
+          end
+          if entry[:credit_bs] != entry_current.credit_bs
+            return @errors << 'No se pueden realizar cambios en algun asiento un día distinto al de creación del comprobante.'
+          end
+          if entry[:debit_sus] != entry_current.debit_sus
+            return @errors << 'No se pueden realizar cambios en algun asiento un día distinto al de creación del comprobante.'
+          end
+          if entry[:credit_sus] != entry_current.credit_sus
+            return @errors << 'No se pueden realizar cambios en algun asiento un día distinto al de creación del comprobante.'
+          end
+          if entry[:account_id] != entry_current.account_id
+            return @errors << 'No se pueden realizar cambios en algun asiento un día distinto al de creación del comprobante.'
+          end
+        end
       end
 
       def add_number
@@ -126,10 +141,6 @@ module Api
                                                        :cycle_id, :company_id, :transaction_type_id,
                                                        entries_attributes: %i[id debit_bs credit_bs debit_sus credit_sus
                                                                               account_id accounting_transaction_id])
-      end
-
-      def accounting_transaction_gloss_params
-        params.require(:accounting_transaction_gloss).permit(:gloss)
       end
     end
   end
